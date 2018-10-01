80e54a7357fe33ecbd9adf105476d7f967f529ad
AnarchyBurger <n.l.schneider@gmail.com>
2018-10-01 08:24:34 -0400

    added tooltips

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 const windy_api = 'N2rcMJYkzqOdQC1od1WrW3OX3i7CUaCB';
+const openWeather = 'fb652d99255894c891ff5c8a796165d3';
 
 const dataSocket = new WebSocket('ws://localhost:9002/connect');
 
@@ -8,44 +9,45 @@ const greenMarker = L.divIcon({ className: 'green-pin' });
 const yellowMarker = L.divIcon({ className: 'yellow-pin' });
 const greyMarker = L.divIcon({ className: 'grey-circle' });
 let c = 0;
+
 dataSocket.addEventListener('message', function(event) {
     let data = JSON.parse(event.data);
 
     if (data.conType === 'LTE') {
-        L.marker([data.loc.lat, data.loc.lon], { icon: yellowMarker })
-            .bindPopup(
-                `Store: #${data.name} <br> ${data.loc.StoreAddress}<br> 
+        let thisMarker = L.marker([data.loc.lat, data.loc.lon], { icon: yellowMarker }).bindPopup(
+            `Store: #${data.name} <br> ${data.loc.StoreAddress}<br> 
                 ${data.loc.City} ${data.loc.State}, 
                 ${data.loc.ZipCode}<p> IP: <a href="https://${data.ip}:8443/">${data.ip}</a> <br>
-                Lat: ${data.loc.lat} | Lon: ${data.loc.lat} <br>Connection: ${data.conType}`
-            )
-            .addTo(markerLayerLTE);
+                Lat: ${data.loc.lat} | Lon: ${data.loc.lat} <br>Connection: ${data.conType}`,
+            { offset: [9, 0] }
+        );
+
+        thisMarker.bindTooltip(`${data.name}`, { direction: 'top', offset: [9, 0] }).addTo(markerLayerLTE);
     }
 
     if (data.conType === 'WAN') {
-        L.marker([data.loc.lat, data.loc.lon], { icon: greenMarker })
-            .bindPopup(
-                `Store: #${data.name} <br> ${data.loc.StoreAddress}<br> 
+        let thisMarker = L.marker([data.loc.lat, data.loc.lon], { icon: greenMarker }).bindPopup(
+            `Store: #${data.name} <br> ${data.loc.StoreAddress}<br> 
                 ${data.loc.City} ${data.loc.State}, 
                 ${data.loc.ZipCode}<p> IP: <a href="https://${data.ip}:8443/">${data.ip}</a> <br>
-                Lat: ${data.loc.lat} | Lon: ${data.loc.lat} <br>Connection: ${data.conType}`
-            )
-            .addTo(markerLayerWAN);
+                Lat: ${data.loc.lat} | Lon: ${data.loc.lat} <br>Connection: ${data.conType}`,
+            { offset: [9, 0] }
+        );
+        thisMarker.bindTooltip(`${data.name}`, { direction: 'top', offset: [9, 0] }).addTo(markerLayerWAN);
     }
 
     if (data.state === 'offline') {
-        L.marker([data.loc.lat, data.loc.lon], { icon: redMarker })
-            .bindPopup(
-                `Store: #${data.name} <br> ${data.loc.StoreAddress}<br> 
+        let thisMarker = L.marker([data.loc.lat, data.loc.lon], { icon: redMarker }).bindPopup(
+            `Store: #${data.name} <br> ${data.loc.StoreAddress}<br> 
             ${data.loc.City} ${data.loc.State}, 
             ${data.loc.ZipCode}<p> IP: <a href="https://${data.ip}:8443/">${data.ip}</a> <br>
-            Lat: ${data.loc.lat} | Lon: ${data.loc.lat} <br>Connection: ${data.state}`
-            )
-            .addTo(markerLayerOffline);
+            Lat: ${data.loc.lat} | Lon: ${data.loc.lat} <br>Connection: ${data.state}`,
+            { offset: [9, 0] }
+        );
+        thisMarker.bindTooltip(`${data.name}`, { direction: 'top', offset: [9, 0] }).addTo(markerLayerOffline);
     }
 });
 
-const openWeather = 'fb652d99255894c891ff5c8a796165d3';
 const markerLayerLTE = L.layerGroup();
 const markerLayerWAN = L.layerGroup();
 const markerLayerOffline = L.layerGroup();
@@ -53,13 +55,12 @@ const markerLayerOffline = L.layerGroup();
 // const darkMap = L.map('darkMap');
 
 const map = L.map('map', {
-    center: [42.9611, -85.6555],
+    center: [38, -87],
     minZoom: 2,
-    zoom: 13,
-    layers: markerLayerLTE,
-    markerLayerWAN,
-    markerLayerOffline
+    zoom: 6,
+    layers: [markerLayerLTE, markerLayerWAN, markerLayerOffline]
 });
+
 const baseMaps = {
     // 'Light Map': lighMap,
     // 'Dark Map': darkMap
